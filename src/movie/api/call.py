5d095c56ba27c620abfa54108b01231aa643c21c
S00zzang <sujinpark8719561@gmail.com>
2024-08-01 17:03:29 +0900

    에휴쯧쯧

--- src/movie/api/call.py
+++ src/movie/api/call.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 def echo(yaho):
-	return yaho
+	return y/tmp/test_parquet/load_dt=20240725/multiMovieYn=Naho
 
 def apply_type2df(load_dt = '20120101', path="~/tmp/test_parquet"):
 	df = pd.read_parquet(f'{path}/load_dt={load_dt}')
@@ -17,9 +17,16 @@ def apply_type2df(load_dt = '20120101', path="~/tmp/test_parquet"):
 
 def save2df(load_dt = '20120101', url_param={}):
 	df = list2df(load_dt, url_param)
+
 	df['load_dt' ] = load_dt
 	# df에 load_dt 컬럼 추가 (조회 일자 YYYYMMDD 형식)
-	# df.to_parquet('~/tmp/test_parquet', partition_cols =['load_dt'])
+	cols = []
+
+	for k,v in url_params.items():
+		df[k] = v
+		cols.append(k)
+
+	df.to_parquet('~/tmp/test_parquet', partition_cols =cols)
 
 	return df
 
